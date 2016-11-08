b0cff346c66feff869496ed8a7637c5aad8a29e3
mikeyabedneyjr <mike.bedney@gmail.com>
2016-11-07 21:56:09 -0800

    Outlined challenge & begun validation class

--- challenge1.py
+++ challenge1.py
@@ -14,5 +14,19 @@
 #    (Hint: the string "\n is the same as pressing the ENTER button)
 
 user_name = raw_input("What is your name? \n")
-user_age = raw_input("And how old are you? \n")
+user_age = int(raw_input("And how old are you? \n"))
 
+# Validate the age is a valid, positive integer. Promt again if not.
+def validateAge(age):
+	while user_age =! int:
+		print "Please enter a number for your age...\n"
+		new_age = int(raw_input("How old are you?\n"))
+		return age
+
+# validateAge(user_age)
+# print user_age
+
+# Calculate the age from 100.  If over 100, send sassy response.
+def whenHundred(user_age):
+	if user_age > 100:
+		print "You're over 100 eh? Good luck with that..."
