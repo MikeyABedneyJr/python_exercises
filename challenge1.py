3674d00c8c1e0972a0040d08293153b0640c0e7e
mikeyabedneyjr <mike.bedney@gmail.com>
2017-03-02 11:04:25 -0800

    minor changes

--- challenge1.py
+++ challenge1.py
@@ -1,10 +1,17 @@
-import = datetime
+import datetime
+
 name = raw_input(“Yo what yo name sucka? ”)
 age = int(raw_input(“just how ancient are you? ”))
 now = datetime.datetime.now()
 
-#calculate when a person will be 100 years old by taking the current year, subtracting their current age then adding 100 years.
+#calculate when a person will be 100 years old by taking the 
+#current year, subtracting their current age then adding 100 
+#years.
 
 def year_when_100(age):
 	when_you_will_be_100 = now.year - age + 100
-	Print ”when_you_will_be_100”
+	return when_you_will_be_100
+
+year_when_100(age) = answer
+
+print "%s, you will be 100 years old in the year %s" % (name, answer)
