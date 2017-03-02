9ae9115b67cf485b46f62e2b91069a3a54b3dbf8
mikeyabedneyjr <mike.bedney@gmail.com>
2017-03-02 10:48:27 -0800

    updating challenge 1

--- challenge1.py
+++ challenge1.py
@@ -1,32 +1,10 @@
-# Challenge 1 from http://www.practicepython.org/
+import = datetime
+name = raw_input(“Yo what yo name sucka? ”)
+age = int(raw_input(“just how ancient are you? ”))
+now = datetime.datetime.now()
 
-# Create a program that asks the user to enter their name and their 
-#  age. Print out a message addressed to them that tells them the year 
-#  that they will turn 100 years old.
+#calculate when a person will be 100 years old by taking the current year, subtracting their current age then adding 100 years.
 
-# EXTRAS:
-
-#  Add on to the previous program by asking the user for another number 
-#    and printing out that many copies of the previous message. 
-#    (Hint: order of operations exists in Python)
-
-#  Print out that many copies of the previous message on separate lines.
-#    (Hint: the string "\n is the same as pressing the ENTER button)
-
-user_name = raw_input("What is your name? \n")
-user_age = int(raw_input("And how old are you? \n"))
-
-# Validate the age is a valid, positive integer. Promt again if not.
-def validateAge(age):
-	while user_age =! int:
-		print "Please enter a number for your age...\n"
-		new_age = int(raw_input("How old are you?\n"))
-		return age
-
-# validateAge(user_age)
-# print user_age
-
-# Calculate the age from 100.  If over 100, send sassy response.
-def whenHundred(user_age):
-	if user_age > 100:
-		print "You're over 100 eh? Good luck with that..."
+def year_when_100(age):
+	when_you_will_be_100 = now.year - age + 100
+	Print ”when_you_will_be_100”
