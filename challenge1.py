0c993f81aeb92a9fae1960e35525d4c2d04e6a4f
mikeyabedneyjr <mike.bedney@gmail.com>
2017-03-07 12:06:10 -0800

    Finished challenge 1 with bonuses

--- challenge1.py
+++ challenge1.py
@@ -1,13 +1,16 @@
 # coding= UTF-8
 import datetime
 
-name = raw_input('Yo what yo name sucka? ')
-age = int(raw_input('Just how ancient are you? '))
+name = raw_input('Yo what yo name sucka?\n>')
+age = int(raw_input('Just how ancient are you?\n>'))
+rub_it_in_number = int(raw_input('How many times do I need to remind you of the answer?\n>'))
 now = datetime.datetime.now()
 
-#calculate when a person will be 100 years old by taking the 
-#current year, subtracting their current age then adding 100 
-#years.
+"""
+calculate when a person will be 100 years old by taking the 
+current year, subtracting their current age then adding 100 
+years.
+"""
 
 def year_when_100(age):
 	when_you_will_be_100 = now.year - age + 99
@@ -15,4 +18,13 @@ def year_when_100(age):
 
 answer = year_when_100(age)
 
-print "%s, you will be 100 years old in the year %s" % (name, answer)
+print ("%s, you will be 100 years old in the year %s\n" % (name, answer))*rub_it_in_number
+
+"""
+1. Add on to the previous program by asking the user for another 
+number and printing out that many copies of the previous 
+message. (Hint: order of operations exists in Python)
+
+2. Print out that many copies of the previous message on separate 
+lines. (Hint: the string "\n is the same as pressing the ENTER button)
+"""
