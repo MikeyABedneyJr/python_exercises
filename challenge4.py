7109d1e5e501009ebbc2bfe679fc30c20192d610
mikeyabedneyjr <mike.bedney@gmail.com>
2017-05-22 11:00:28 -0700

    Fixed bug in challenge 4

--- challenge4.py
+++ challenge4.py
@@ -5,16 +5,10 @@ list of all the divisors of that number.
 
 list = []
 
-def get_user_number():
-  
-  user_number = raw_input("Give me a whole number and I will tell you what numbers will divide into it evenly\n:")
-  
-  try:
-    user_integer = int(user_number)
-  except ValueError:
-    print "You did not enter a whole number.\n\n\n"
-    get_user_number()
 
+
+def get_user_number(user_integer):
+  
   x = range(1, user_integer)
 
   for elem in x:
@@ -24,5 +18,17 @@ def get_user_number():
   # Added so that the user number will actually appear in the list of answers.
   list.append(user_integer)
 
-get_user_number()
+def is_number_an_integer():
+  user_number = raw_input("Give me a whole number and I will tell you what numbers will divide into it evenly\n:")
+
+  try:
+    user_integer = int(user_number)
+    get_user_number(user_integer)
+  except ValueError:
+    print "You did not enter a whole number.\n\n\n"
+    is_number_an_integer()
+
+
+is_number_an_integer()
+
 print list
