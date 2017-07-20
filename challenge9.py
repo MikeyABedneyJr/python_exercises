c74409b797d1f6744f79cc3a38e63e5f03bf6aa8
mikeyabedneyjr <mike.bedney@gmail.com>
2017-07-20 10:37:15 -0700

    finished basics of challenge 9 and purposely did better with adding comments

--- challenge9.py
+++ challenge9.py
@@ -13,26 +13,53 @@ import random
 from random import randint
 import time
 
+# Generate secret number, set a counter, explain the game, and pause. p=0 is used to exit 
+# the while loop gracefully rather than throwing an error.
 secret_number = random.randint(1, 9)
 print "I'm thinking of a number between 1 & 9 (including 1 & 9)."
 p = 0
-time.sleep(1,5)
+time.sleep(1.5)
+counter = 0
 
 while (p < 1):
-	print ("Current secret number is: %s" % secret_number)
-	user_guess = int(raw_input("Whaddya think it is?\nYour guess: "))
 
+	# The print statement below is for testing purposes
+	# print ("Current secret number is: %s" % secret_number)
+	
+	user_guess = int(raw_input("Whaddya think it is?\nYour guess: "))
+	
+	# The print statement below helped to see if the counter was working correctly
+	# print "You are currently on guess number: %s" % counter
+
+	time.sleep(1.5)
+	
+	# Now they are on their first guess. If they get it wrong, this will add another
+	# +1 to the counter to keep track of their guesses
+	counter += 1
+
+	# Evaluate the number that is guessed & give feedback
 	if user_guess < secret_number:
 		print "Toooooooo looooooooow"
-
+		
 	elif user_guess > secret_number:
 		print "Toooooooo hiiiiiiigh"
 
+
 	else:
-		print "You won!"
+		print ("You won! Number of guesses: %s" % counter)
+		time.sleep(1.5)
+
+		# Ask user if they want to play again
 		play_again = raw_input("If you want to quit this game, type EXIT. Otherwise press enter to play again.\n").lower()
+
+		# Stop the game on 'exit'
 		if (play_again == 'exit'):
 			p = p + 1
+			print "Oh I get it...we're done here then. It's fine. I'll be fine...just go...."
+		
+		# User chooses to play again, make a new number and reset the counter. While loop
+		# continues...
 		else:
 			secret_number = random.randint(1, 9)
+			counter = 0
 			print "A new secret number between 1 & 9 has been chosen!"
