ba0c607112f71e38e51641757f522fbf89c81503
mikeyabedneyjr <mike.bedney@gmail.com>
2017-09-14 11:07:31 -0700

    Started and finished challenge 13

---
+++ challenge13.py
@@ -0,0 +1,21 @@
+'''
+Write a program that asks the user how many Fibonnaci numbers to generate and then generates them. Take this opportunity to think about how you can use functions. Make sure to ask the user to enter the number of numbers in the sequence to generate.(Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, ...)
+'''
+
+# Ask user for length of Fibonnaci list
+user_list_length = int(raw_input("How many Fibonnaci numbers would you like me to generate?"))
+
+# Make the function to generate the list: a + (a - 1)
+def fibonnachi_list(list_length):
+	count = 1
+	first = 0
+	second = 1
+	print second
+	while count <= (list_length - 1):
+		current_fibonnaci_number = first + second
+		print current_fibonnaci_number
+		first = second
+		second = current_fibonnaci_number
+		count += 1
+
+fibonnachi_list(user_list_length)
